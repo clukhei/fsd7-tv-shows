69fe187ac1438be40ba58d4c5b9f20e8733e94ba
Lu Khei <kheiboardwarrior@gmail.com>
2020-11-04 13:55:03 +0800

    deploy to heroku

--- main.js
+++ main.js
@@ -13,7 +13,7 @@ app.use(express.static(__dirname + "/static"));
 
 const pool = mysql.createPool({
 	host: process.env.DB_HOST || "localhost",
-	port: parseInt(process.env.DB_PORT) || 3306,
+	port: parseInt(process.env.PORT) || 3306,
 	database: process.env.DB_NAME || "leisure",
 	user: process.env.DB_USER,
 	password: process.env.DB_PASSWORD,
